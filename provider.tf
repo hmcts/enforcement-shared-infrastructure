439da79763d91a2e8debe80331e40dbce048be42
JakeCG <jakecgowler@gmail.com>
2025-03-14 09:49:31 +0000

    HDPI-180 - Add shared infrastructure repository with a key vault (#1)
    * HDPI-180 - Add shared infrastructure repository with a key vault

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.22.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {}
+}
